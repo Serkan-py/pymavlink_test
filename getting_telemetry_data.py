b98778f1be684e056af3476cb6fb4f12280ebc97
Serkan BÜTÜN <63592375+Serkan-py@users.noreply.github.com>
2023-06-27 21:31:49 +0300

    Update getting_telemetry_data.py
    The Turkish description and variables have been translated into English.

--- getting_telemetry_data.py
+++ getting_telemetry_data.py
@@ -1,55 +1,52 @@
-# BU KOD TELEMETRI KOMUTLARININ NASIL CALISTIGINI TEST ETMEK AMACIYLA SERKAN BUTUN TARAFINDAN YAZILMISTIR 
-
+                           # This code has been written by Serkan Butun to test how telemetry commands work.
 
 import sys, time , math, datetime 	
 from pymavlink import mavutil,mavwp
 
 
-# baglanti olusturma kismi
-#master = mavutil.mavlink_connection("/dev/serial0", baud=57600) #serial baglanti yapildiginda kullanilacak olan baglanti
-#master = mavutil.mavlink_connection("/dev/ttyACM0", baud=115200, wait_ready=True) #serial baglanti yapildiginda kullanilacak olan baglanti
-master = mavutil.mavlink_connection('0.0.0.0:14551',wait_ready=True) #simulasyon ile baglanti yapildiginda kullanilacak olan baglanti
+# create connection
+#master = mavutil.mavlink_connection("/dev/serial0", baud=57600) #The connection to be used when establishing a serial connection.
+#master = mavutil.mavlink_connection("/dev/ttyACM0", baud=115200, wait_ready=True) #The connection to be used when establishing a serial connection.
+master = mavutil.mavlink_connection('0.0.0.0:14551',wait_ready=True) #The connection to be used when establishing a connection with simulation.
 master.wait_heartbeat()
-print('baglandi')
+print('connected')
 
 master.mav.request_data_stream_send(master.target_system, master.target_component, mavutil.mavlink.MAV_DATA_STREAM_ALL, 2, 1)
 
-def telemetri():
+def telemetry():
     msg = master.recv_match(blocking=True)
     if msg.name == 'GLOBAL_POSITION_INT':
-        IHA_irtifa = msg.relative_alt/1000
-        print(" IHA_irtifa = ", IHA_irtifa)
+        UAV_altitude = msg.relative_alt/1000
+        print(" UAV_altitude = ", UAV_altitude)
     
     
     msg = master.recv_match(blocking=True)
     if msg.name == 'ATTITUDE':
-        IHA_dikilme = math.degrees(msg.pitch)
-        IHA_yonelme = math.degrees(msg.yaw)
-        IHA_yatis = math.degrees(msg.roll)
-        print(" IHA_dikilme = ", IHA_dikilme)
-        print(" IHA_yonelme = ", IHA_yonelme)
-        print(" IHA_yatis = ", IHA_yatis)
+        UAV_pitch = math.degrees(msg.pitch)
+        UAV_yaw = math.degrees(msg.yaw)
+        UAV_roll = math.degrees(msg.roll)
+        print(" UAV_pitch = ", UAV_pitch)
+        print(" UAV_yaw = ", UAV_yaw)
+        print(" UAV_roll = ", UAV_roll)
     
     
     msg = master.recv_match(blocking=True)
     if msg.name == 'GPS_RAW_INT':
         print(datetime.datetime.fromtimestamp(msg.time_usec / 1000.0, tz=datetime.timezone.utc))
         print((datetime.datetime.fromtimestamp(msg.time_usec / 1000.0, tz=datetime.timezone.utc).strftime('%H:%M:%S.%f')))
-        IHA_enlem = msg.lat / 1.0e7
-        IHA_boylam = msg.lon / 1.0e7
-        IHA_hiz = msg.vel/100
-        print(" IHA_enlem = ", IHA_enlem)
-        print(" IHA_boylam = ", IHA_boylam)
-        print(" IHA_hiz = ", IHA_hiz)
+        UAV_latitude = msg.lat / 1.0e7
+        UAV_longtitude = msg.lon / 1.0e7
+        UAV_velocity = msg.vel/100
+        print(" UAV_latitude = ", UAV_latitude)
+        print(" UAV_longtitude = ", UAV_longtitude)
+        print(" UAV_velocity = ", UAV_velocity)
     
     
     msg = master.recv_match(blocking=True)
     if msg.name == 'BATTERY_STATUS':
-        IHA_batarya = msg.battery_remaining
-        print(" IHA_batarya = ", IHA_batarya)
+        UAV_battery = msg.battery_remaining
+        print(" UAV_battery = ", UAV_battery)
 
 
 while True:
-    telemetri()
-
-
+    telemetry()
